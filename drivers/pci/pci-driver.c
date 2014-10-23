6d5a5c83233715f4546594b65faf234117e8d50d
kin-cheng <kin912@gmail.com>
2014-10-23 10:24:47 +0800

    Revert "Squashed update of kernel from 3.4.69 to 3.4.70"
    This reverts commit c50c0490d343f55c875d3eff55532c67976275ef.

--- drivers/pci/pci-driver.c
+++ drivers/pci/pci-driver.c
@@ -680,7 +680,6 @@ static int pci_pm_suspend(struct device *dev)
 		goto Fixup;
 	}
 
-	pci_dev->state_saved = false;
 	if (pm->suspend) {
 		pci_power_t prev = pci_dev->current_state;
 		int error;
@@ -815,7 +814,6 @@ static int pci_pm_freeze(struct device *dev)
 		return 0;
 	}
 
-	pci_dev->state_saved = false;
 	if (pm->freeze) {
 		int error;
 
@@ -904,7 +902,6 @@ static int pci_pm_poweroff(struct device *dev)
 		goto Fixup;
 	}
 
-	pci_dev->state_saved = false;
 	if (pm->poweroff) {
 		int error;
 
@@ -1016,7 +1013,6 @@ static int pci_pm_runtime_suspend(struct device *dev)
 	if (!pm || !pm->runtime_suspend)
 		return -ENOSYS;
 
-	pci_dev->state_saved = false;
 	error = pm->runtime_suspend(dev);
 	suspend_report_result(pm->runtime_suspend, error);
 	if (error)
